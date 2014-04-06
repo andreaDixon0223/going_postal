6cee953ff38046496fce094e30aa712ff7dfb09f
Andrea Dixon <andrea.coa.dixon@gmail.com>
2014-04-06 09:02:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoingPostal::Application.config.secret_token = '3662f6421a1c9bebb540c46aa3189273cfdec6acada064f88d9592fe145fcd2cc8abb8daa4ebc5d8b59dd66689c8d42fa831c7565604590f657905b18f288b7d'
